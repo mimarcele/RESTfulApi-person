6df9fb0b452715db545f58505a3c11c8eec5ad68
Milena Gomes <milena.gomes.silva@everis.com>
2020-10-03 19:41:04 -0300

    adicionando packages e classe principal controller

---
+++ src/test/java/com/example/apirestful/personapi/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apirestful.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
